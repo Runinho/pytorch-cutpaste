c16ce5afec02c92d8603ddcad7724edb94fcd142
Runinho <rune@frohn.cologne>
2021-07-22 10:18:47 +0200

    optimizing repeat

--- dataset.py
+++ dataset.py
@@ -3,6 +3,18 @@ from pathlib import Path
 from PIL import Image
 from joblib import Parallel, delayed
 
+class Repeat(Dataset):
+    def __init__(self, org_dataset, new_length):
+        self.org_dataset = org_dataset
+        self.org_length = len(self.org_dataset)
+        self.new_length = new_length
+
+    def __len__(self):
+        return self.new_length
+
+    def __getitem__(self, idx):
+        return self.org_dataset[idx % self.org_length]
+
 class MVTecAT(Dataset):
     """Face Landmarks dataset."""
 
